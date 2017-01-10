e0dbe525150d4782d785aa75f8197e48a9089ea1
TanujaBanda <tanujakumari24m@gmail.com>
2017-01-10 18:43:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function addition(){
+function findSum(){
 	 sum=0;
 	for(var i=0;i<arguments.length;i++){
 		sum+=parseInt(arguments[i]);
@@ -7,4 +7,4 @@ function addition(){
 	}
 var sum;
 
-console.log("the sum is = "+addition("23","34","45"));
+console.log("the sum is = "+findSum("23","34","45"));
